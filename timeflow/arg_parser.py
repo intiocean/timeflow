b535001ad6599e82cf8c500fba9d55d8440ccce4
Ocean <iocean@macl0005.maninvestments.ad.man.com>
2016-05-06 11:04:30 +0100

    Add an example to the help message

--- timeflow/arg_parser.py
+++ timeflow/arg_parser.py
@@ -106,7 +106,38 @@ def set_stats_parser(subparser):
 
 
 def parse_args(args):
-    parser = argparse.ArgumentParser()
+    general_usage = """
+General usage
+-------------
+When arriving in the morning to start the timer
+    tf log
+
+Logging time
+    tf log "project: a commit message for my time"
+    tf log project
+    tf log "A log message without a project will get grouped into a project called Other"
+    tf log "Coffee**: the asterisks at the end of the project mark this as a non-work"
+    log"
+
+Editing the time log
+    tf edit
+
+Showing time spent
+    tf stats
+
+For further options just run add --help to any of the above commands.
+
+Opinionated usage
+-----------------
+The way I use it is that I have a category for each of my ongoing work/projects and then a few other categories
+* Meeting
+* Email
+* Support: only for actual support
+* Team: team discussions, questions, interuptions, fixing things for the team, etc
+* Code Review: I copy a snippet from the URL of the PR to identify code reviews. eg. tools_bin/pull-requests/46
+
+If its related to a Jira I add #EQ-1234 in the message"""
+    parser = argparse.ArgumentParser(description=general_usage, formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument("-v", "--version",
                         help="Show timeflow's version",
